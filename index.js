f15c9b2de0b23231a66c4df508cb64a4574104c4
Pablo Garnham <pgarham@uc.cl>
2021-11-29 17:00:36 -0300

    adding cors allow any

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const opsDocuments = require('./routes/opsDocuments');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        " x-client-secret, Authorization, Origin, X-Requested-With, Content-Type"
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+  });
+
 app.use(express.json());
 
 app.use('/op-documents', opsDocuments);
