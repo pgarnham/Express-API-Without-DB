ba820bfbfb168c033f9960594a99519891bb51f0
Pablo Garnham <pgarham@uc.cl>
2021-12-08 22:23:25 -0300

    Cleaning files

--- functions/getOpDocuments.js
+++ functions/getOpDocuments.js
@@ -3,20 +3,10 @@ axios = require('axios');
   const getOpDocuments = async (opDI, pmAddress) => {
     let requestUrl = process.env.GOOGLE_SCRIPT_API + opDI + '&pm_address=' + pmAddress;
     let documentURL
-    /*await axios({
-        method: 'get',
-        url: requestUrl,
-        headers: {'Content-Length': 0},
-      }).then((response) => {
-        return response
-      }, (error) => {
-        return error
-      }
-      )*/
-      await axios
-      .get(requestUrl)
-      .then((res) => {documentURL = res.data.documentURL})
-      .catch((err) => console.log(err));
+    await axios
+    .get(requestUrl)
+    .then((res) => {documentURL = res.data.documentURL})
+    .catch((err) => console.log(err));
     return documentURL
     }
   module.exports = getOpDocuments;
--- routes/opsDocuments.js
+++ routes/opsDocuments.js
@@ -3,19 +3,6 @@ var router = express.Router();
 let getOpDocuments = require('../functions/getOpDocuments');
 
 
-const clients = [
-  { name: 'João', age: 20 },
-  { name: 'Maria', age: 10 },
-  { name: 'José', age: 50 },
-  { name: 'Pedro', age: 45 },
-  { name: 'Lucas', age: 25 }
-];
-
-const client = {
-  name: 'Lucas', 
-  age: 25
-}
-
 router.get('/', async function(req, res) {
   let opId = parseInt(req.query.op_id);
   let token = req.query.token;
@@ -28,16 +15,5 @@ router.get('/', async function(req, res) {
   res.send({documentURL: response});
 });
 
-router.post('/', function(req, res) {
-  res.send(client);
-});
-
-router.put('/', function(req, res) {
-  res.send(client);
-});
-
-router.delete('/', function(req, res) {
-  res.send(204);
-});
 
 module.exports = router;
